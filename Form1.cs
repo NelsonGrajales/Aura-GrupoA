acf96c6e8fb3c050d0586c5e3f7656c245474729
Kavmuri <151587029+Kavmuri@users.noreply.github.com>
2025-10-20 18:35:09 -0500

    Add Form1 class for login application

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Windows.Forms;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOGINAPP;
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        this.Dispose();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        this.Dispose();
+    }
+    }
+
